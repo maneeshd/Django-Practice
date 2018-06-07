263b96ab0135927907f03fba123492cda6208faf
maneeshd <maneeshd77@gmail.com>
2018-06-07 19:30:29 +0530

    Blog Enhancements: Added Tag management

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 # Create your Model Managers here.
@@ -71,6 +72,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+    tags = TaggableManager()
 
     class Meta:
         """
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,11 +1,40 @@
-from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, redirect
-from .models import Post, Comment
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
+from .models import Post
+from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 
 
 # Create your views here.
+def post_list(request, tag_slug=None):
+    """
+    List the posts in the home page (3 posts per page)
+    """
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)   # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                   'posts': posts,
+                                                   'tag': tag})
+
+
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = "posts"
@@ -42,12 +71,18 @@ def post_detail(request, year, month, day, slug):
     if sent:
         request.session["sent"] = False
         request.session["sent"] = None
-    print(sent, recipient)
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+    
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
                                                      'comment_form': comment_form,
                                                      'sent': sent,
-                                                     'recipient': recipient})
+                                                     'recipient': recipient,
+                                                     'similar_posts': similar_posts})
 
 
 def share_post(request, post_id):
