f870cf4fdef03f7e98408eee3967fc57f49a4714
maneeshd <maneeshd77@gmail.com>
2018-06-08 19:34:18 +0530

    Blog Final: Added RSS Feed feature

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-
 from . import views
+from .feeds import LatestPostsFeed
 
 
 app_name = 'blog'
@@ -12,4 +12,5 @@ urlpatterns = [
     url(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', views.post_detail, name='post_detail'),
     url(r'^(?P<post_id>\d+)/share/$', views.share_post, name='share_post'),
+    url(r'^feed/$', LatestPostsFeed(), name='post_feed'),
 ]
